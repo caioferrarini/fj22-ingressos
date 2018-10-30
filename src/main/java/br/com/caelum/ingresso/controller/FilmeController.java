a7eae8d9caf107423c29eb966791e48d2a93bfb2
HubertFer <humbertofernandezsosa@gmail.com>
2018-10-30 19:51:33 -0300

    Detalhes dos Filmes Json em API externa

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -20,6 +20,7 @@ import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.rest.OmdbClient;
 
 /**
  * Created by nando on 03/03/17.
@@ -99,9 +100,15 @@ public class FilmeController {
 	   Filme filme = filmeDao.findOne(id);
 	   List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
 	   
+	   Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme);
+	   
 	   modelAndView.addObject("sessoes", sessoes);
+	   modelAndView.addObject("detalhes", detalhesDoFilme.orElse(new DetalhesDoFilme()));
 	   
 	   return modelAndView;
    }
+   
+   @Autowired
+   private OmdbClient client;
 
 }
